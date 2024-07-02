f2bbfd9272f9dd6892e84f46d35aa4e834436cbb
fcd199 <facu_me@hotmail.com>
2024-07-02 18:14:50 -0400

    update path

--- src/scrap.py
+++ src/scrap.py
@@ -2,7 +2,7 @@ import requests
 import csv
 from module.module1 import get_urls,append_scraped_urls
 from module.module3 import get_article,get_local_path,save_csv
-from param import final_url,file_path
+from param import final_url,raw_data_path
 
 urls_to_scrape = get_urls(final_url)
 for post_url in urls_to_scrape:
@@ -10,4 +10,4 @@ for post_url in urls_to_scrape:
     if processed is not None:
         local_path = get_local_path(processed)
         save_csv(processed, local_path)
-append_scraped_urls(reversed(urls_to_scrape),file_path)
+append_scraped_urls(reversed(urls_to_scrape),raw_data_path)
