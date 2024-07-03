67823c08afaf5e3545e81bf8eb860adf166677ff
fcndata <127878852+fcndata@users.noreply.github.com>
2024-07-03 12:43:41 -0400

    Update scrap.py

--- src/scrap.py
+++ src/scrap.py
@@ -2,12 +2,12 @@ import requests
 import csv
 from module.module1 import get_urls,append_scraped_urls
 from module.module3 import get_article,get_local_path,save_csv
-from param import new_url_path,raw_data_path
-
+from param import new_url_path,scrapped_url_path
 urls_to_scrape = get_urls(new_url_path)
 for post_url in urls_to_scrape:
     processed = get_article(post_url)
     if processed is not None:
         local_path = get_local_path(processed)
         save_csv(processed, local_path)
-append_scraped_urls(reversed(urls_to_scrape),raw_data_path)
+append_scraped_urls(reversed(urls_to_scrape),scrapped_url_path)
+
