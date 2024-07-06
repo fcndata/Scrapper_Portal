180205a803b2e1da285cd1abd82dae78c6950796
fcndata <127878852+fcndata@users.noreply.github.com>
2024-07-06 14:23:36 -0400

    Update module.file

--- src/scrap.py
+++ src/scrap.py
@@ -1,7 +1,7 @@
 import requests
 import csv
-from module1 import get_urls_to_scrape,append_scraped_urls
-from module3 import get_article,get_local_path,save_csv
+from module.module1 import get_urls_to_scrape,append_scraped_urls
+from module.module3 import get_article,get_local_path,save_csv
 from param import fieldnames
 
 urls_to_scrape = get_urls_to_scrape()
