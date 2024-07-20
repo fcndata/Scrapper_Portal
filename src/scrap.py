e822adc65f299c3229600dfe8832eef480269d99
fcd199 <facu_me@hotmail.com>
2024-07-20 09:46:22 -0400

    update path

--- src/scrap.py
+++ src/scrap.py
@@ -1,9 +1,11 @@
-import requests
-import csv
+import sys
+import os
 from data import fill_raw_db
 from module import get_urls_to_scrape,append_scraped_urls
 from module import get_article,get_local_path,save_csv 
-from module import fieldnames 
+from module import fieldnames
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 urls_to_scrape = get_urls_to_scrape()
 for post_url in urls_to_scrape:
