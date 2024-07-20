bef0c647c4da2cd27645cfc9806eeb012974e9ad
fcd199 <facu_me@hotmail.com>
2024-07-20 14:45:29 -0400

    check_raw_data

---
+++ data/check_raw_data.py
@@ -0,0 +1,19 @@
+import sqlite3
+from data import db_file_path
+
+def check_raw_data():
+    conn = sqlite3.connect(db_file_path)
+    cursor = conn.cursor()
+    
+    # Ejecutar la consulta para obtener los datos de la tabla 'raw'
+    cursor.execute("SELECT * FROM raw")
+    rows = cursor.fetchall()
+    
+    conn.close()
+    
+    # Imprimir los datos obtenidos
+    for row in rows:
+        print(row)
+
+if __name__ == "__main__":
+    check_raw_data()
